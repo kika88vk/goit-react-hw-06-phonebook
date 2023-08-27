51494c9996acc1e3467997f75afaf89c75848716
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-08-27 22:19:24 +0300

    add redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+const persistConfig = {
+    key: 'contacts',
+    storage,
+}
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer)
 
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer,
+        contacts: persistedContactsReducer,
         filter: filterReducer,
     }
 })
+
+export const persistor = persistStore(store)
